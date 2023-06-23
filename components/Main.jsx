a5d5116c533f43a78d04736ebe4ef6cf3f768054
irfansanuri <irfansanuri98@gmail.com>
2023-06-24 02:35:50 +0800

    remove profile image

--- components/Main.jsx
+++ components/Main.jsx
@@ -11,10 +11,10 @@ const Main = () => {
     <div id='home' className='w-full h-screen text-center'>
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div>
-          <div className='w-[200px] lg:w-[250px] h-[200px] lg:h-[250px]  m-auto shadow-xl shadow-gray-400 
-          rounded-full flex items-center justify-center p-2 mb-8'>
+          {/* <div className='w-[200px] lg:w-[250px] h-[200px] lg:h-[250px]  m-auto shadow-xl shadow-gray-400 
+          rounded-full flex items-center justify-center p-2 mb-8 '>
             <Image src={ProfileImg} className='rounded-full' alt='/' />
-          </div>
+          </div> */}
           <p className='uppercase text-sm tracking-widest text-gray-600'>
             LET&#39;S BUILD SOMETHING TOGETHER
           </p>
