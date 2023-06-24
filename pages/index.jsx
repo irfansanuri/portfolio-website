02cd56f7be3cbe52e44a2b5d960bbc4a32dd6510
irfansanuri <irfansanuri98@gmail.com>
2023-06-25 06:08:29 +0800

    Bash commit

---
+++ pages/index.jsx
@@ -0,0 +1,169 @@
+import Head from 'next/head'
+import About from "@/src/components/About";
+import Blog from "@/src/components/Blog";
+import Contact from "@/src/components/Contact";
+import Skills from "@/src/components/Skills";
+import Testiminails from "@/src/components/Testiminails";
+import { tony } from "@/src/layouts/utils";
+import { Fragment, useEffect, useState } from "react";
+
+import Services from "@/src/components/Services";
+import TypingAnimation from "@/src/components/TypingAnimation";
+import Footer from "@/src/layouts/Footer";
+import dynamic from "next/dynamic";
+
+const Projects = dynamic(() => import("@/src/components/Projects"), {
+  ssr: false,
+});
+
+const Home = () => {
+  useEffect(() => {
+    tony.scrollToActiveNav();
+  }, []);
+  const [toggle, setToggle] = useState(false);
+  return (
+    <Fragment>
+      <Head>
+        {/* metas */}
+        <meta charSet="utf-8" />
+        <meta name="author" content="themepaa" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta name="keywords" content="Irfan - Portfolio Template" />
+        <meta name="description" content="Irfan - Portfolio Template" />
+        {/* title */}
+        <title>Irfan - Front-End Web Developer</title>
+      </Head>
+      <div className="mob-header">
+        <div className="d-flex">
+          <div className="navbar-brand">
+            <a className="logo-text" href="index.html">
+              Irfan
+            </a>
+          </div>
+          <button className="toggler-menu" onClick={() => setToggle(!toggle)}>
+            <span />
+            <span />
+            <span />
+          </button>
+        </div>
+      </div>
+
+      <header
+        className={`header-left ${toggle ? "menu-open menu-open-desk" : ""}`}
+        id="navbar-collapse-toggle"
+      >
+        <div className="navbar-brand">
+          <a className="logo-text" href="index.html">
+            Irfan
+          </a>
+        </div>
+        <ul className="nav nav-ul">
+          <li>
+            <a className="nav-link" href="#home">
+              <i className="fas fa-house-damage" />
+              <span>Home</span>
+            </a>
+          </li>
+          <li>
+            <a className="nav-link" href="#about">
+              <i className="far fa-address-card" />
+              <span>About Me</span>
+            </a>
+          </li>
+          <li>
+            <a className="nav-link" href="#services">
+              <i className="fas fa-concierge-bell" />
+              <span>Services</span>
+            </a>
+          </li>
+          <li>
+            <a className="nav-link" href="#projects">
+              <i className="fas fa-briefcase" />
+              <span>Portfolio</span>
+            </a>
+          </li>
+          <li>
+            <a className="nav-link" href="#blog">
+              <i className="fas fa-blog" />
+              <span>Blog</span>
+            </a>
+          </li>
+          <li>
+            <a className="nav-link" href="#contactus">
+              <i className="fas fa-id-card-alt" />
+              <span>Contact</span>
+            </a>
+          </li>
+        </ul>
+      </header>
+
+      <main className="main-left">
+        {/* Home Banner */}
+        <section
+          id="home"
+          className="home-banner-01 bg-cover bg-center bg-no-repeat"
+          style={{ backgroundImage: "url(static/img/home-banner.jpg)" }}
+        >
+          <div className="container">
+            <div className="row full-screen align-items-center p-100px-tb">
+              <div className="col-md-6">
+                <div className="ht-text">
+                  <h6>Hello there...</h6>
+                  <h1>Irfan Sanuri</h1>
+                  <h2>
+                    I'm a <TypingAnimation />
+                  </h2>
+                  <p>
+                    I’m focused on building responsive front-end web
+                    applications integrating back-end technologies.
+                  </p>
+                  <div className="btn-bar go-to">
+                    <a className="m-btn m-btn-theme" href="#projects">
+                      my work
+                    </a>
+                    <a className="m-btn m-btn-t-theme" href="#contactus">
+                      Hire Me
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="go-to go-to-next">
+            <a href="#about">
+              <span />
+            </a>
+          </div>
+        </section>
+        {/* End Home Banner */}
+
+        {/* End Home Banner */}
+        {/* about us */}
+        <About />
+        {/* end about us */}
+        {/* fun */}
+        <Skills />
+        {/* End fun */}
+        {/* resume */}
+        <Services />
+        {/* End resume */}
+        {/* Work */}
+        <Projects />
+        {/* End work */}
+        {/* Testiminails */}
+        <Testiminails />
+        {/* End Testiminails */}
+        {/* Blog */}
+        <Blog />
+        {/* End Blog */}
+        <Contact />
+      </main>
+      <Footer />
+    </Fragment>
+  );
+};
+export default Home;
---
+++ src/components/Skills.jsx
@@ -0,0 +1,62 @@
+import { useEffect } from "react";
+import { tony } from "../layouts/utils";
+import Image from "next/image";
+import HtmlCss from "../../public/assets/skills/html-css.svg";
+import Javascript from '../../public/assets/skills/javascript.png';
+import ReactImg from '../../public/assets/skills/react.png';
+import Bootstrap from '../../public/assets/skills/bootstrap.png';
+import Github from '../../public/assets/skills/github.png';
+import Node from '../../public/assets/skills/node.png';
+import NextJS from '../../public/assets/skills/nextjs.png'
+import AWS from '../../public/assets/skills/aws.png';
+
+
+const skillSet = [
+  { id: 0, logo: HtmlCss, name: "HTML/CSS" },
+  { id: 1, logo: Javascript, name: "JavaScript" },
+  { id: 2, logo: Bootstrap, name: "Bootstrap" },
+  { id: 3, logo: ReactImg, name: "React" },
+  { id: 4, logo: NextJS, name: "Next" },
+  { id: 5, logo: AWS, name: "AWS" },
+  { id: 6, logo: HtmlCss, name: "Vercel" },
+  { id: 7, logo: Github, name: "Github" },
+];
+
+const Skills = () => {
+  useEffect(() => {
+    tony.activeSkillProgress();
+  }, []);
+  return (
+    <section id="skills" className="section">
+      <div className="container">
+        <div className="row sm-m-25px-b m-20px-b">
+          <div className="col-md-12">
+            <div className="section-title">
+              <h3 className="dark-color text-uppercase">My Skills</h3>
+              <p className="text-uppercase small">What I Can Do</p>
+            </div>
+          </div>
+        </div>
+        <div className="row justify-content-between">
+          <div className="col-lg-12 m-15px-tb">
+            <div className="row">
+              {skillSet.map((skill) => (
+                <div key={skill.id} className="col-sm-12 col-md-6 col-lg-3 m-15px-tb">
+                  <div className="feature-box-2 media align-items-center">
+                    <div className="logo">
+                      <Image src={skill.logo} width={64} height={64} alt={skill.name} />
+                    </div>
+                    <div className="media-body">
+                      <h5>{skill.name}</h5>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Skills;
