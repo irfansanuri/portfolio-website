b522baca60e34d88ce1ca112a9a6525da046e249
irfansanuri <irfansanuri98@gmail.com>
2023-06-24 10:46:23 +0800

    navbar not fixed unscroll

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
       className={
         shadow
           ? 'fixed w-full h-20 shadow-xl z-[100] ease-in-out duration-300'
-          : 'fixed w-full h-20 z-[100]'
+          : 'w-full h-20 z-[100]'
       }
     >
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
