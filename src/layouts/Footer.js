0f5b5e02aa8c279adbb8f160313d4a0501f06c06
irfansanuri <irfansanuri98@gmail.com>
2023-06-25 14:37:49 +0800

    Update Footer.js

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -5,25 +5,25 @@ const Footer = () => {
         <div className="row align-items-center">
           <div className="col-md-6 m-10px-tb">
             <div className="nav justify-content-center justify-content-md-start">
-              <a href="#">
-                <i className="fab fa-facebook-f" />
-              </a>
-              <a href="#">
-                <i className="fab fa-twitter" />
-              </a>
-              <a href="#">
-                <i className="fab fa-instagram" />
-              </a>
-              <a href="#">
+              <a
+                href="https://www.linkedin.com/in/irfansanuri/"
+                target="_blank"
+              >
                 <i className="fab fa-linkedin-in" />
               </a>
+              <a href="https://github.com/irfansanuri" target="_blank">
+                <i className="fab fa-github" />
+              </a>
+              <a href="#contactus">
+                <i className="fas fa-envelope" />
+              </a>
               <a href="#">
-                <i className="fab fa-pinterest" />
+                <i className="fas fa-user" />
               </a>
             </div>
           </div>
           <div className="col-md-6 m-10px-tb text-center text-md-right">
-            <p>© {new Date().getFullYear()} copyright all right reserved</p>
+            <p>© {new Date().getFullYear()} Irfan Sanuri</p>
           </div>
         </div>
       </div>
